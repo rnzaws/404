dc6265d41080d05beedeb0770c96307288134a76
Ryan <ryannitz@amazon.com>
2017-07-17 09:04:11 -0400

    Initial

---
+++ 404.go
@@ -0,0 +1,34 @@
+/**
+ * Any code, applications, scripts, templates, proofs of concept,
+ * documentation and other items are provided for illustration purposes only.
+ *
+ * (C) Copyright 2017 Amazon Web Services
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		io.WriteString(w, "404 Not Found")
+	})
+
+	http.ListenAndServe(":8404", nil)
+}
